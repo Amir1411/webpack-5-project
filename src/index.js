d248a7ddffc9d0582ba0722188279623cd62780f
“Mohd <md.amir14@gmail.com>
2021-12-01 14:49:42 +0530

    add babel to project

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ const person = {
     last_name: "Amir",
     email: "md.amir14@gmail.com"
 }
+const person1 = {
+    ...person,
+    email: "md.amir1411@gmail.com"
+}
 console.log(person);
+console.log(person1);
