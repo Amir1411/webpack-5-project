156c07826714be0b6d191cae3e736598bc3cd3a9
“Mohd <md.amir14@gmail.com>
2021-12-01 16:41:20 +0530

    Add react to project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import './style/index.scss';
-const person = {
-    first_name: "Mohd",
-    last_name: "Amir",
-    email: "md.amir14@gmail.com"
-}
-const person1 = {
-    ...person,
-    email: "md.amir1411@gmail.com"
-}
-console.log(person);
-console.log(person1);
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('root'));
+
